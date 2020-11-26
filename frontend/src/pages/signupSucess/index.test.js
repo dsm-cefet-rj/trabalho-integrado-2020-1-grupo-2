5b1db70178ec5c5a4c1789465fa4a8221a3f4f8d
RicardoBucard <ricardobucard@gmail.com>
2020-11-26 17:45:46 -0300

    Casos de defeito de conta e tela de singup

---
+++ frontend/src/pages/signupSucess/index.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+describe("Sistema valida usuário e senha", function() {
+
+    test('Mensagem de sucesso e redireciona para Dashboard', () => { throw 'Not implemented yet' });
+
+})
