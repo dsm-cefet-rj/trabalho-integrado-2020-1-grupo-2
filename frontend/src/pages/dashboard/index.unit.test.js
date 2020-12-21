3cfff10cec7d62d2e08be1a96e520a92955fc3da
Ricardo de Castro <ricardobucard@gmail.com>
2020-12-20 22:37:16 -0300

    Adiciona teste da tela de dashboard

---
+++ frontend/src/pages/dashboard/index.unit.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import NotasCidade from './index';
+
+describe('Features de dashboard', function(){
+	
+	test('acessa pagina dashboard', ()=>{
+		
+        render(<NotasCidade />, { wrapper: MemoryRouter });
+
+		expect(screen.getByText(/Adicionar Cidade/i)).toBeInTheDocument();
+		expect(screen.getByText(/Deletar Todas as Cidades/i)).toBeInTheDocument();
+		expect(screen.getByText(/Conta/i)).toBeInTheDocument();
+
+	});
+	
+
+});
